bf05cdf929c2ee0cb58df81ec15d2e2e7e3bd175
Amir TOLY <amir.toly@gmail.com>
2015-03-31 18:42:35 +0200

    Avoid a SELECT query by setting fetch attribute to JOIN

--- src/main/java/org/hibernate/tutorial/EventManager.java
+++ src/main/java/org/hibernate/tutorial/EventManager.java
@@ -93,7 +93,6 @@ public class EventManager {
         session.beginTransaction();
 
         Person aPerson = (Person) session.load(Person.class, personId);
-        // adding to the emailAddress collection might trigger a lazy load of the collection
         aPerson.getEmailAddresses().add(emailAddress);
 
         session.getTransaction().commit();
